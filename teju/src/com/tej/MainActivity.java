789b1d27c482d1fcb5af8906cd2ebd87fa9394a6
KUSHMA <KUSHMA@DESKTOP-6F2CT0U>
2023-09-30 18:43:33 +0530

    tnsifjavaprogram

---
+++ teju/src/com/tej/MainActivity.java
@@ -0,0 +1,28 @@
+package com.tej;
+
+public class MainActivity {
+	
+	    public static void main(String[] args) {
+	    	int n=10;
+	    	int m=30;
+	    	int k=3;
+	        for (n = 10; n <= m; n++) {
+	            int digitSum = get(n);
+	            if (digitSum == k) {
+	                System.out.println(n);
+	            }
+	        }
+	    }
+
+	    // Function to calculate the sum of digits in a number
+	    public static int get(int num) {
+	        int sum = 0;
+	        while (num != 0) {
+	            sum += num % 10;
+	            num /= 10;
+	        }
+	        return sum;
+	    }
+	}
+
+
